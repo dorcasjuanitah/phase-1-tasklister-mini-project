202d120c9b8c98074d2710c9fd2fca5da20a9ed8
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-04-02 16:34:41 +0300

    adds todolist in a form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener('submit', (e) =>{
+    e.preventDefault();
+   buildToDo(e.target.new-task-description.value);
+  })
 });
+
+function buildToDo(todo){
+  //console.log(todo);
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.tectcontent = 'X';
+  btn.addEventListener('click', handleDelete);
+
+  p.textContent = `${todo} `;
+  document.querySelector('#new-task-description').appendChild(p)
+}
+
+function handleDelete(e){
+  e,target.parentNode.remove;
+}
